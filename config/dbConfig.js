0c12591ee0ea8202e0ab609c02d64cb3dd638b61
ashwani <abic.ashwani@gmail.com>
2024-11-24 14:18:21 +0530

    jjf

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,6 +5,8 @@ const dbConnection = () => {
         mongoose.connect(process.env.DB_URL).then(() => {
             console.log("Databse is connected successfullt")
         }).catch((error) => {
+
+
             console.log(`Something went wrong while connecting to the database ${process.env.DB_URL}`)
         })
     } catch (error) {
